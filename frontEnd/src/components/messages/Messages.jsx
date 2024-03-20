84842619992c76a7bdc5f965409f2883f0fbbe44
grellonss <fabio.grelloni@gmail.com>
2024-03-20 21:18:28 +0100

    1.5

--- frontEnd/src/components/messages/Messages.jsx
+++ frontEnd/src/components/messages/Messages.jsx
@@ -42,40 +42,40 @@ const Messages = () => {
 };
 
 export default Messages; */
-
-import { useEffect, useRef } from 'react'
-import Message from './Message'
+import React, { useEffect, useRef } from 'react';
+import Message from './Message';
 import useGetMessages from '../../hooks/useGetMessages';
 import MessageSkeleton from '../skeletons/MessageSkeleton';
-import useListenMessages from '../../hooks/useListenMessages';
+import useConversation from '../../zustand/useConversation';
 
 const Messages = () => {
-  const {messages,loading} = useGetMessages();
-  useListenMessages();
+  const { messages, loading } = useGetMessages();
+  const { selectedConversation } = useConversation();
   const lastMessageRef = useRef();
-  
+
   useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
-    }, 100); // Impostato un timeout di 100 millisecondi
-  
-    return () => clearTimeout(timeoutId);
-  },[messages]);
+    lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+
   return (
     <div className='px-4 flex-1 overflow-auto'>
-        {!loading  && messages.length > 0 && messages.map((message) => (
-          <div key={message._id} ref={lastMessageRef}>
-            <Message  message={message} />
-            </div>
-        )) }
-        {loading && [...Array(3)].map((_,idx) => <MessageSkeleton key={idx} />)}
+      {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
 
-        {!loading && messages.length === 0 && (
-          <p className='text-center'>Send a message to start the conversation</p>
-        )}
+      {!loading && selectedConversation && messages.map((message) => (
+        <div key={message._id} ref={message._id === messages[messages.length - 1]._id ? lastMessageRef : null}>
+          <Message message={message} />
+        </div>
+      ))}
+
+      {!loading && selectedConversation && messages.length === 0 && (
+        <p className='text-center'>Send a message to start the conversation</p>
+      )}
     </div>
   );
-};export default Messages;
+};
+
+export default Messages;
+
 
 /* import { useEffect, useRef } from 'react'
 import Message from './Message'
